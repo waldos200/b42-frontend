8945e11e71bc4e3af8fb85ca6dd840472a8f182a
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-09 19:51:38 -0600

    agregar barra de navegacion con reactstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Navigation from './components/Navigation/index';
 
 function App() {
   return (
-    <h1>Hola Cinta Negra</h1>
+    <React.Fragment>
+      <Navigation />
+      <div className="container">
+        <h1>Hola Cinta Negra</h1>
+      </div>
+    </React.Fragment>
   );
 }
 
