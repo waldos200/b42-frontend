4d4f4ae02fd58b64ba54eaa5a9587f798f8019e8
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-09 20:28:10 -0600

    quite div

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -18,42 +18,40 @@ const Navigation = (props) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggle = () => setIsOpen(!isOpen);
-    
+
     return (
-        <div>
-            <Navbar color="light" light expand="md">
-                <NavbarBrand href="/">reactstrap</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/components/">Components</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                        </NavItem>
-                        <UncontrolledDropdown nav inNavbar>
-                            <DropdownToggle nav caret>
-                                Options
+        <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/components/">Components</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                    </NavItem>
+                    <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            Options
               </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem>
-                                    Option 1
+                        <DropdownMenu right>
+                            <DropdownItem>
+                                Option 1
                 </DropdownItem>
-                                <DropdownItem>
-                                    Option 2
+                            <DropdownItem>
+                                Option 2
                 </DropdownItem>
-                                <DropdownItem divider />
-                                <DropdownItem>
-                                    Reset
+                            <DropdownItem divider />
+                            <DropdownItem>
+                                Reset
                 </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                    <NavbarText>Simple Text</NavbarText>
-                </Collapse>
-            </Navbar>
-        </div>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>
+                </Nav>
+                <NavbarText>Simple Text</NavbarText>
+            </Collapse>
+        </Navbar>
     );
 }
 
