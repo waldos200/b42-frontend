13bd095ff96a8450707d0686dc3467287ae78885
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-10 18:57:26 -0600

    crear routs y llamarlos en la app inicial

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import Navigation from './components/Navigation/index';
-import HolaMundo from './components/HolaMundo/index';
+import React, { Fragment } from 'react';
+
+// Components
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+
+// Contexts
 import AuthContextProvider from './contexts/AuthContext';
+
+// Misc
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Container } from 'reactstrap';
+import Routes from './Routes';
 
 function App() {
   return (
-    <AuthContextProvider>
-      <Navigation />
-      <div className="container">
-        <h1>Hola Cinta Negra</h1>
-        <HolaMundo />
-      </div>
-    </AuthContextProvider>
+    <Fragment>
+      <Router>
+        <AuthContextProvider>
+            <Navigation />
+            <Container className="mt-4">
+              <Switch>
+                { Routes }
+              </Switch>
+            </Container>
+            <Footer />
+        </AuthContextProvider>
+      </Router>
+    </Fragment>
   );
 }
 
