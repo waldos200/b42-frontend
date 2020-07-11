b3b8e9ff68639ea414d0ff81b92e72ce7bd5a67a
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-10 19:27:10 -0600

    agregar logica de estad con interfaz

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,  
   Form,  
@@ -8,14 +8,31 @@ import {
 } from 'reactstrap';
 
 const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    const jsonSend = {
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      password
+    };
+    console.log(jsonSend);
+  }
 
   return (
     <React.Fragment>
-      <h1 className="mb-4">Signup on Maui App</h1>
-      <Form>
+      <h1 className="mb-4">Signup on Waldo App</h1>
+      <Form onSubmit={handleForm}>
         <FormGroup>
           <Label>First Name</Label>
           <Input 
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
             type="text"
             id="firstName"
             name="inputFirstName" 
@@ -24,6 +41,8 @@ const Signup = () => {
         <FormGroup>
           <Label>Last Name</Label>
           <Input
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
             type="text" 
             name="lastName" 
             id="inputLastname" 
@@ -32,6 +51,8 @@ const Signup = () => {
         <FormGroup>
           <Label>Email</Label>
           <Input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             type="email"
             name="email"
             id="exampleEmail"
@@ -40,6 +61,8 @@ const Signup = () => {
         <FormGroup>
           <Label>Password</Label>
           <Input 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
             name="password"
             id="examplePassword"
