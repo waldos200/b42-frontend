217fb1b68d4f546d1a292993aa52a79e544e70a7
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-13 19:47:31 -0600

    agregar useEffects

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,10 +1,18 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = (props) => {  
   const [token, setToken] = useState('');
   const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    const item = localStorage.getItem('maui_token');
+    if(item) {
+      setToken(item);
+      setIsAuth(true);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ 
